761ef8ba0a6d35aeb9bb9689500415f956bc3690
unknown <eva.schwarzl@htl-salzburg.ac.at>
2019-12-04 11:35:26 +0100

    Created ScoreExample Script in Unity

---
+++ Assets/MyGame/Scripts/ScoreExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetScore(9, "Ice");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetScore(5, "Pete");
+    }
+
+    public void SetScore(int score, string name)
+    {
+        int scoreValue = score;
+        string nameValue = name;
+        Debug.Log("score=" + scoreValue + " name=" + nameValue);
+    }
+}
